90fc228a014bea0c15b938571286a58bca7de245
AlexGronskiy <85637138+AlexGronskiy@users.noreply.github.com>
2024-05-20 09:52:25 +0300

    Update main.py

--- task_6/main.py
+++ task_6/main.py
@@ -12,26 +12,26 @@ logging.basicConfig(level=logging.INFO,
                               logging.StreamHandler()])
 
 
-def create_example_file_if_not_exists(f_path):
-    """
-    Створює файл з заданим шляхом і записує в нього деякий текст, якщо файл не існує.
-    """
-    try:
-        # Спробуємо відкрити файл для читання
-        with open(f_path, 'r') as file:
-            logging.info(f"Файл {f_path} вже існує. Продовження роботи програми.")
-    except FileNotFoundError:
-        # Якщо файл не знайдено, створюємо його
-        content = """
-Деякий текст
-Текст можно додати в будь який час відкрив example.txt
-та прописав в ньому більше тексту.
-Цей текст сгенерован автоматично як перший вхідний текст.
-Кінець прикладу."""
-
-        with open(f_path, 'w') as file:
-            file.write(content)
-        logging.info(f"Файл {f_path} створено з вмістом.")
+#def create_example_file_if_not_exists(f_path):
+#    """
+#    Створює файл з заданим шляхом і записує в нього деякий текст, якщо файл не існує.
+#    """
+#    try:
+#        # Спробуємо відкрити файл для читання
+#        with open(f_path, 'r') as file:
+#            logging.info(f"Файл {f_path} вже існує. Продовження роботи програми.")
+#    except FileNotFoundError:
+#        # Якщо файл не знайдено, створюємо його
+#        content = """
+#Деякий текст
+#Текст можно додати в будь який час відкрив example.txt
+#та прописав в ньому більше тексту.
+#Цей текст сгенерован автоматично як перший вхідний текст.
+#Кінець прикладу."""
+#
+#        with open(f_path, 'w') as file:
+#            file.write(content)
+#        logging.info(f"Файл {f_path} створено з вмістом.")
 
 
 def read_file(f_path):
@@ -55,7 +55,7 @@ def read_file(f_path):
 
 # Приклад використання:
 file_path = 'example.txt'
-create_example_file_if_not_exists(file_path)
+#create_example_file_if_not_exists(file_path)
 data = read_file(file_path)
 if data is not None:
     print(data)
